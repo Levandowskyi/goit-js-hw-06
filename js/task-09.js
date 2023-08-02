72d7743959ad95a226f4e7a5b0a350c290d5c5df
Levandowskyi <stcorvin@gmail.com>
2023-08-02 11:41:09 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,15 @@
+const colorBtn = document.querySelector('.change-color')
+const body = document.querySelector('body')
+const viewColor = document.querySelector('.color')
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+colorBtn.addEventListener('click', function () {
+  let randomColor = getRandomHexColor()
+  body.style.background = randomColor
+  viewColor.textContent = randomColor
+})
