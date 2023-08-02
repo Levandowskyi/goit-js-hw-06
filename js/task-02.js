ecf9f26a35853f01b5952bf86f0f0691215f440e
Levandowskyi <stcorvin@gmail.com>
2023-08-02 09:54:18 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const elementsOfLi = [];
+
+
+ingredients.forEach((ingredient) => {
+  const li = document.createElement("li");
+  li.textContent = ingredient;
+  li.classList.add("item");
+  elementsOfLi.push(li);
+});
+
+
+const ingredientsOfUl = document.getElementById("ingredients");
+
+
+ingredientsOfUl.append(...elementsOfLi);
